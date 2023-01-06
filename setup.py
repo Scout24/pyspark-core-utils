bb38b495c344c7f79762a715e62a9051da45afd6
Christian Dietze <christian.dietze@scout24.com>
2023-01-06 12:59:52 +0100

    Update Spark and Delta versions

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pyspark-core-utils",
-    version="1.1.1",
+    version="1.3.0",
     author="ImmobilienScout24",
     description="PySpark core utils library",
     long_description=long_description,
