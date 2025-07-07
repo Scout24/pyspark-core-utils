8760cd965a28cd46a83e73b3ca1fa78161cb06d6
arseni-ivankovich <arseni.ivankovich@innowise.com>
2025-07-07 12:52:45 +0200

    test chnges

--- src/pyspark_core_utils/apps.py
+++ src/pyspark_core_utils/apps.py
@@ -7,10 +7,10 @@ import pyspark_core_utils.configuration as configuration_service
 class SparkApp:
     def __init__(self, app_package: Package):
         self.app_package = app_package
-        self.environment = configuration_service.get_environment()
-        self.config = configuration_service.get_configuration(self.app_package,
-                                                              f'config/app-config-{self.environment}.yaml')
-        self.logger = configuration_service.get_logger(self.app_package, log_into_file=True)
+        #self.environment = configuration_service.get_environment()
+        #self.config = configuration_service.get_configuration(self.app_package,
+        #                                                      f'config/app-config-{self.environment}.yaml')
+        #self.logger = configuration_service.get_logger(self.app_package, log_into_file=True)
         self.spark = self._init_spark()
 
     def execute(self):
